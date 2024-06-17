d69f80b469a8737c9b7d87743b9dfc6b913a2198
GowthamNagarajanEvolus <160841806+GowthamNagarajanEvolus@users.noreply.github.com>
2024-06-17 23:02:48 +0530

    added await
    added await

--- test/specs/retainLevel_orderCancellation/level3_silver_retainLevel_netsuite_orderCancellation.js
+++ test/specs/retainLevel_orderCancellation/level3_silver_retainLevel_netsuite_orderCancellation.js
@@ -7,6 +7,6 @@ const memberLevel = netsuiteTestdata.silver;
 describe(`Netsuite_orderCancellation_retainLevel_${memberLevel}_${accountId}`, () => {
 
   it(`netsuite_orderCancellation_retainLevel_${memberLevel}_${accountId}`, async () => {
-    netsuitePage.orderCancellation(accountId);
+    await netsuitePage.orderCancellation(accountId);
   });
 });
--- test/specs/upgradeLevel_orderCancellation/ach_upgradeLevel_netsuite_orderCancellation.js
+++ test/specs/upgradeLevel_orderCancellation/ach_upgradeLevel_netsuite_orderCancellation.js
@@ -6,7 +6,7 @@ const accountId = netsuiteTestdata.upgrade_AccountId_ach;
 describe(`Netsuite_orderCancellation_upgradeLevel_ach_${accountId}`, () => {
 
   it(`netsuite_orderCancellation_upgradeLevel_ach_${accountId}`, async () => {
-    netsuitePage.orderCancellation(accountId);
+    await netsuitePage.orderCancellation(accountId);
   });
 
 
